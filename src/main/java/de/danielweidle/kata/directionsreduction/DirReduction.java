ebf0e9d7b15231e2cd3a05dac8baa2e816a66a22
Daniel Weidle <daniel.weidle@adesso.de>
2019-01-28 08:44:23 +0100

    Stub directions reduction kata

---
+++ src/main/java/de/danielweidle/kata/directionsreduction/DirReduction.java
@@ -0,0 +1,11 @@
+package de.danielweidle.kata.directionsreduction;
+
+/**
+ * https://www.codewars.com/kata/directions-reduction/train/java
+ */
+public class DirReduction {
+    public static String[] dirReduc(final String[] arr) {
+        // Your code here.
+        return new String[] {};
+    }
+}
