ffc747ff05bee3583c5dc2140180a7b01bd92f25
Daniel Weidle <dw@danielweidle.de>
2020-06-09 21:57:33 +0200

    Refactored bouncing balls

--- codewars-kotlin/src/main/kotlin/de/danielweidle/codewars/bouncingballs/BouncingBall.kt
+++ codewars-kotlin/src/main/kotlin/de/danielweidle/codewars/bouncingballs/BouncingBall.kt
@@ -10,20 +10,12 @@ fun assertInput(h: Double, bounce: Double, window: Double) {
  * https://www.codewars.com/kata/5544c7a5cb454edb3c000047
  */
 fun bouncingBall(h: Double, bounce: Double, window: Double): Int {
-    print("height=${h}, bounce=${bounce}, window=${window}")
     try {
         assertInput(h, bounce, window)
     } catch (e: IllegalArgumentException) {
         return -1
     }
-    var height: Double = h
-    var dropsHigherThanWindow: Int = 0
-    while (height > window) {
-        dropsHigherThanWindow++
-        height = height.times(bounce)
-        if (height > window) {
-            dropsHigherThanWindow++
-        }
-    }
-    return dropsHigherThanWindow
+    return generateSequence(h) { it * bounce }
+            .takeWhile { it > window }
+            .count() * 2 - 1
 }
